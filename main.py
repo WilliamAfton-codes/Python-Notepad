47cfdc701971aff9593cef9cb3dea1795c811414
William Afton <107783820+WilliamAfton-codes@users.noreply.github.com>
2022-11-12 14:20:17 +0000

    Added file editing

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import os.path
 import time
 
 def process():
@@ -9,15 +10,24 @@ def process():
         if lines == "exit-editor\n":
             break
         else:
-            fo.seek(0)
             fo.writelines(lines)
         
 if __name__ == "__main__":
     print("Welcome to Python-Notepad! Made with <3 by WilliamAfton-codes\n")
     
     filename_input = str(input("What would you like the file to be called?: "))
-    ext_input = str(input("What would you like the extension to be? (Don't type the '.'): "))
+    print("Don't type the '.' in the extension")
+    ext_input = str(input("What would you like the extension to be? (Leave blank to default to .txt): "))
+    if ext_input == "":
+        ext_input = "txt"
+    else:
+        pass
     filename = filename_input + "." + ext_input
+    
+    file_exists = os.path.exists(filename)
+    if file_exists == True:
+        print("A file with this name in this directory already exists, Python-Notepad will add text to the end")
+    
     fo = open(filename, "a")
     
     print("Loading text editor...")
